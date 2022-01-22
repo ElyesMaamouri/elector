17a51e33470d7a155c00ac0e417a202722d09c2b
ElyesMaamouri <elyesmaamouri@gmail.com>
2022-01-22 13:49:44 +0100

    Add update user

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,5 @@ module.exports = (app, pathApi) => {
   const userControl = require("../controllers/userControl");
   app.get(pathApi + "/users", userControl.listUser_get);
   app.delete(pathApi + "/users/:id", userControl.removeUser_delete);
+  app.patch(pathApi + "/users/:id", userControl.updateUser_patch);
 };
